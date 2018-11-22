0ceddfac06cdb6266aea147e8fd9a6332a50ab83
Usuario Local <chequilin3@gmail.com>
2018-11-22 19:39:24 +0100

    Starting development

---
+++ Clon Bricks'n'balls/Assets/Scripts/BloqueVida.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloqueVida : MonoBehaviour {
+
+    public int vida;
+    TextMesh textHijo;
+	// Use this for initialization
+	void Start () {
+        textHijo = gameObject.GetComponentInChildren<TextMesh>();
+        textHijo.text = vida.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Ball")
+        {
+            vida--;
+            textHijo.text = vida.ToString();
+            if (vida <= 0)
+                Destroy(gameObject);
+        }
+    }
+
+}
